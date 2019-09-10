952963b2b13f9b3e84fbb4de0a92621e1c351264
luke <lukevance90@gmail.com>
2019-09-09 22:53:47 -0600

    added api request to get espn data

--- handler.js
+++ handler.js
@@ -4,6 +4,11 @@ const serverless = require('serverless-http');
 const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const bodyParser = require('body-parser');
+const R = require('ramda');
+
+const env = require('./.env.json');
+
+const {getBoxscore} = require('./espn');
 
 const app = express();
 
@@ -19,8 +24,21 @@ const responder = (res, data) => {
 
 // MAIN ENTRY point for app
 app.post('/handler', async (req, res) => {
-  const message = "Welcome to Fantasy Football Textbot!";
-  responder(res, {text: message});
+  if (req.body.From === env.phone){
+    const boxscore = await getBoxscore(env.leagueId, env.teamId);
+    const isYourTeam = homeAway => homeAway.teamId == boxscore.team.id;
+    const youHomeAway = Object.keys(R.filter(isYourTeam, boxscore.boxscore))[0];
+    const oppHomeAway = Object.keys(R.filter(!isYourTeam, boxscore.boxscore))[0];
+    const message = 
+      `Score update for team ${boxscore.team.abbrev}
+
+      You: ${boxscore.boxscore[youHomeAway].totalPoints}
+      Opponent: ${boxscore.boxscore[oppHomeAway].totalPoints}
+      `;
+      responder(res, {text: message});
+  } else {
+    responder(res, {text: "Welcome to Fantasy Football Textbot!"});
+  }
 });
 
 const appHandler = serverless(app);
